abdba633300fabc8f32276dce560f10d0e15e6e2
BaagEcoder <gbatbold17@gmail.com>
2023-03-03 19:19:07 +0800

    updatepage

---
+++ Js/index.js
@@ -0,0 +1,21 @@
+const header = ` <header>
+<nav id="navbar">
+    <a class="logo" href="homePage.html">
+        <img src="./img/logo3.png" alt="">
+    </a>
+    <ul class="Pages">
+        <li><a href="homePage.html"> Нүүр хуудас </a></li>
+        <li><a href="./pages/mentor.html">Менторууд</a></li>
+        <li><a href="./pages/Class-Structure.html">Сургалтууд</a></li>
+        <li><a href="./pages/members.html">Сурагчид</a></li>
+        <li class="login1"><a href="homePage.html">Нэвтрэх</a></li>
+
+    </ul>
+
+</nav>
+</header>`;
+const hd = document.querySelector('.header');
+hd.innerHTML = `<p>a</p>`
+console.log(hd);
+// hd.innerHTML = header
+// hd.innerHTML = `<div>Baagi</div>`
