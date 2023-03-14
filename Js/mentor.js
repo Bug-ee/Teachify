90d804ac0125bfe960e5b8dfb06f224c4bcac6b7
betazaya <vbzaya@gmail.com>
2023-03-14 17:15:20 +0800

    last

--- Js/mentor.js
+++ Js/mentor.js
@@ -1,16 +1,41 @@
 const names = [
-  { name: "Заяадэлгэр", who: "teacher" },
-  { name: "Тэмүүжин", who: "teacher" },
-  { name: "Намсрай", who: "teacher,mentor" },
-  { name: "Тодсүрэн", who: "teacher,mentor" },
-  { name: "Цэрэнтогтох", who: "teacher,mentor" },
+  {
+    name: "Заяадэлгэр",
+    who: "teacher",
+    videourl:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2023/02/23142013/Alaskan-Malamute-puppy-laying-down-outdoors.jpg",
+  },
+  {
+    name: "Тэмүүжинs",
+    who: "teacher",
+    videourl:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2023/02/23142013/Alaskan-Malamute-puppy-laying-down-outdoors.jpg",
+  },
+  {
+    name: "Намсрай",
+    who: "teacher,mentor",
+    videourl:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2023/02/23142013/Alaskan-Malamute-puppy-laying-down-outdoors.jpg",
+  },
+  {
+    name: "Тодсүрэн",
+    who: "teacher,mentor",
+    videourl:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2023/02/23142013/Alaskan-Malamute-puppy-laying-down-outdoors.jpg",
+  },
+  {
+    name: "Цэрэнтогтох",
+    who: "teacher,mentor",
+    videourl:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2023/02/23142013/Alaskan-Malamute-puppy-laying-down-outdoors.jpg",
+  },
 ];
 const result = document.querySelector(".section1");
 document.addEventListener("DOMContentLoaded", function () {
   const shownames = names
     .map((el) => {
       return `
-        <button class="card" onclick="play()">
+        <button class="card" onclick="play('  ${el.videourl} ')">
         <div class="name">${el.name}</div>
         <div class="who">${el.who}</div>
         </button>
@@ -19,23 +44,28 @@ document.addEventListener("DOMContentLoaded", function () {
     .join("");
   result.innerHTML = shownames;
 });
-const introduction = [
-  {
-    videourl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/DotEDU_domain_logo.svg/1200px-DotEDU_domain_logo.svg.png",
-    intro: "nbnbnbnbnb",
-  },
-];
 
-function play() {
+function play(videourl) {
+  console.log("sss");
+  console.log(videourl);
   const run = document.querySelector(".section2");
-  console.log("dsfsddsf");
-  const showvideos = introduction.map((el) => {
-    return `
+  // console.log("dsfsaaddsf");
+  const showvideos = `
   <div class="video">
-  <img src="${el.videourl}"/>
-  <div class="text">${el.intro}</div>
+  <p>${videourl}</p>
+  <img src="${videourl}"/>
   </div>`;
-  });
+
   run.innerhtml = showvideos;
+  console.log(showvideos);
 }
+
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
+
+// fetch("http://localhost:3000/hello", requestOptions)
+//   .then((response) => response.text())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log("error", error));
