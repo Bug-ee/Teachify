f3463ba43029a3729e5725a450565d717726b630
BaagEcoder <gbatbold17@gmail.com>
2023-03-14 20:20:08 +0800

    pull hgeerei guys

--- Js/mentor.js
+++ Js/mentor.js
@@ -1,16 +1,41 @@
 const names = [
-  { name: "Заяадэлгэр", who: "teacher" },
-  { name: "Тэмүүжин", who: "teacher" },
-  { name: "Намсрай", who: "teacher,mentor" },
-  { name: "Тодсүрэн", who: "teacher,mentor" },
-  { name: "Цэрэнтогтох", who: "teacher,mentor" },
+  {
+    name: "Заяадэлгэр",
+    who: "teacher",
+    videourl:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2023/02/23142013/Alaskan-Malamute-puppy-laying-down-outdoors.jpg",
+  },
+  {
+    name: "Тэмүүжинs",
+    who: "teacher",
+    videourl:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2023/02/23142013/Alaskan-Malamute-puppy-laying-down-outdoors.jpg",
+  },
+  {
+    name: "Намсрай",
+    who: "teacher,mentor",
+    videourl:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2023/02/23142013/Alaskan-Malamute-puppy-laying-down-outdoors.jpg",
+  },
+  {
+    name: "Тодсүрэн",
+    who: "teacher,mentor",
+    videourl:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2023/02/23142013/Alaskan-Malamute-puppy-laying-down-outdoors.jpg",
+  },
+  {
+    name: "Цэрэнтогтох",
+    who: "teacher,mentor",
+    videourl:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2023/02/23142013/Alaskan-Malamute-puppy-laying-down-outdoors.jpg",
+  },
 ];
 const result = document.querySelector(".section1");
 document.addEventListener("DOMContentLoaded", function () {
   const shownames = names
     .map((el) => {
       return `
-        <button class="card" onclick="play()">
+        <button class="card" onclick="play('  ${el.videourl} ')">
         <div class="name">${el.name}</div>
         <div class="who">${el.who}</div>
         </button>
@@ -19,23 +44,27 @@ document.addEventListener("DOMContentLoaded", function () {
     .join("");
   result.innerHTML = shownames;
 });
-const introduction = [
-  {
-    videourl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/DotEDU_domain_logo.svg/1200px-DotEDU_domain_logo.svg.png",
-    intro: "nbnbnbnbnb",
-  },
-];
 
-function play() {
+function play(videourl) {
   const run = document.querySelector(".section2");
-  console.log("dsfsddsf");
-  const showvideos = introduction.map((el) => {
-    return `
+  const showvideos = names
+    .map((element) => {
+      return `
   <div class="video">
-  <img src="${el.videourl}"/>
-  <div class="text">${el.intro}</div>
+  <p>${element.videourl}</p>
+  <img src="${element.videourl}"/>
   </div>`;
-  });
+    })
+    .join("");
   run.innerhtml = showvideos;
 }
+
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
+
+fetch("http://localhost:50/more", requestOptions)
+  .then((response) => response.json())
+  .then(result) => console.log(result)
+  .catch((error) => console.log("error", error));
